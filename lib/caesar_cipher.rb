604b5b6da936879a4bd4458b5f3bca26d39490ab
keflood <kathryneflood@gmail.com>
2016-02-02 22:05:25 -0600

    refactored caesar cipher

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -2,6 +2,7 @@ class CaesarCipher
 
 #a-z range [97..122]
 #A-Z range [65..90]
+#symbols range [0..64] or [91..96] or [123..127]
 
   def encrypt(string, shift)
     if string == ""
@@ -12,41 +13,101 @@ class CaesarCipher
     end
   end
 
+  def decrypt(string, shift)
+    if string == ""
+      ""
+
+    else
+      string_characters = string.chars
+      string_characters.map { |char| decrypt_char(char, shift)}.join
+end
+end
+
   def encrypt_char(char, shift)
     ascii_code = char.ord
-    #if ascii_code == (0..64) or (91..96) or (123..127)
 
-    shifted_ascii_code = ascii_code + shift
-      if ascii_code === (97..122) or (65..90)
-
-      #  shifted_ascii_code = ascii_code + shift
-      #  shifted_ascii_code.chr
-      #else shifted_ascii_code = ascii_code + 0
-        #shifted_ascii_code.chr
-
-     #if (97..122).include?(ascii_code)
+    if letter?(ascii_code)
+        shifted_ascii_code = ascii_code + shift
 
       if shift > 26
         remains = shift % 26
-        if (97..122).include?(ascii_code)
-        shifted_ascii_code = 97 + remains
-      else shifted_ascii_code = 65 + remains
-      end
+        shifted_ascii_code = ascii_code + remains
       end
 
-      if shifted_ascii_code > 122
-          remainder = shifted_ascii_code % 122
-          shifted_ascii_code = 96 + remainder
-        end
-
-        shifted_ascii_code.chr
+      if out_of_range?(ascii_code, shifted_ascii_code)
+        remainder = shifted_ascii_code % (end_of_range(ascii_code) + 1)
+        shifted_ascii_code = start_of_range(ascii_code) + remainder
       end
+
+      else
+        shifted_ascii_code = ascii_code
+
+      end
+          shifted_ascii_code.chr
     end
 
-    #def start_value
-    #  if (97..122).include?(ascii_code)
-    #    start = 97
-    #  else
-    #    start = 65
-    #end
- end
+    def decrypt_char(char, shift)
+      ascii_code = char.ord
+
+      if letter?(ascii_code)
+          shifted_ascii_code = ascii_code - shift
+
+        if shift > 26
+          remains = shift % 26
+          shifted_ascii_code = ascii_code - remains
+        end
+
+        if out_of_range_decrypt?(ascii_code, shifted_ascii_code)
+          remainder = (start_of_range(ascii_code) - 1) % shifted_ascii_code
+          shifted_ascii_code = end_of_range(ascii_code) - remainder
+        end
+
+          shifted_ascii_code.chr
+
+        end
+      end
+
+private
+
+  def letter?(ascii_code)
+    lowercase?(ascii_code) || uppercase?(ascii_code)
+  end
+
+  def out_of_range?(ascii_code, shifted_ascii_code)
+
+    letter?(ascii_code) && shifted_ascii_code > end_of_range(ascii_code)
+
+    end
+
+    def out_of_range_decrypt?(ascii_code, shifted_ascii_code)
+
+      letter?(ascii_code) && shifted_ascii_code < start_of_range(ascii_code)
+
+      end
+
+def start_of_range(ascii_code)
+  if lowercase?(ascii_code)
+    97
+  else
+    65
+  end
+end
+
+def end_of_range(ascii_code)
+  if lowercase?(ascii_code)
+    122
+  else
+    90
+  end
+end
+
+def lowercase?(ascii_code)
+  (97..122).include?(ascii_code)
+end
+
+def uppercase?(ascii_code)
+  (65..90).include?(ascii_code)
+end
+
+
+end
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -12,13 +12,14 @@ describe CaesarCipher do
     expect(caesar_cipher.encrypt("a", 0)).to eq "a"
   end
 
-  it "can shift a character with a shift of 1" do
+  context "encrypts a single character" do
+  it "using a shift of 1" do
     expect(caesar_cipher.encrypt("a", 1)).to eq "b"
   end
-
-  it "can shift a character with a shift of 2" do
+  it "using a shift of 2" do
     expect(caesar_cipher.encrypt("a", 2)).to eq "c"
   end
+end
 
   it "can shift two characters with a shift of 1" do
     expect(caesar_cipher.encrypt("ab", 1)).to eq "bc"
@@ -28,33 +29,65 @@ describe CaesarCipher do
     expect(caesar_cipher.encrypt("abc", 2)).to eq "cde"
   end
 
-  it "encrypts 'z' with a shift factor of 1 to be 'a'" do
+  context "encrypts loops from 'z' to 'a' regardless of case" do
+   it "encrypts 'z' with a shift factor of 1 to be 'a'" do
     expect(caesar_cipher.encrypt("z", 1)).to eq "a"
   end
-
-  context "encrypts 'z' as upper case or lower case" do
   it "encrypts 'z' with a shift factor of 2 to be 'b'" do
     expect(caesar_cipher.encrypt("z", 2)).to eq "b"
   end
   it "encrypts 'Z' with a shift factor of 2 to be 'B'" do
     expect(caesar_cipher.encrypt("Z", 2)).to eq "B"
   end
+  it "encrypts 'xyz' with a shift of 3 to be 'abc'" do
+    expect(caesar_cipher.encrypt('xyz', 3)).to eq "abc"
+end
 end
 
   it "encrypts 'A' with a shift of 1" do
     expect(caesar_cipher.encrypt("A", 1)).to eq "B"
   end
 
-  context "can take a shift great than 26" do
+  context "can take a shift greater than 26" do
   it "can return a shifted lowercase character" do
     expect(caesar_cipher.encrypt("a", 27)).to eq "b"
   end
   it "can return a shifted uppercase character" do
     expect(caesar_cipher.encrypt("A", 27)).to eq "B"
   end
+  it "can return multiple characters" do
+    expect(caesar_cipher.encrypt("abc", 27)).to eq "bcd"
   end
+end
 
-  #it "can return a symbol without shifting" do
-    #expect(caesar_cipher.encrypt("&", 1)).to eq "&"
-  #end
+it "can encrypt a mixed case string" do
+  expect(caesar_cipher.encrypt("aBcD", 1)).to eq "bCdE"
+end
+
+context "can decrypt" do
+it "given a character with a shift of 1" do
+  expect(caesar_cipher.decrypt("b", 1)).to eq "a"
+end
+it "can decrypt 'a' and loop to 'z'" do
+  expect(caesar_cipher.decrypt("a", 1)).to eq "z"
+end
+it "can decrypt 'A' and loop to 'Y'" do
+  expect(caesar_cipher.decrypt("A", 2)).to eq "Y"
+end
+it "can take a shift larger than 26" do
+  expect(caesar_cipher.decrypt("b", 27)).to eq "a"
+end
+it "can decrypt multiple characters" do
+  expect(caesar_cipher.decrypt("cde", 2)).to eq "abc"
+end
+#it "decrypts 'abc' with a shift of 3 to be 'xyz'" do
+#  expect(caesar_cipher.encrypt("abc", 3)).to eq "xyz"
+#end
+end
+
+
+
+  it "can return a symbol without shifting" do
+    expect(caesar_cipher.encrypt("&", 1)).to eq "&"
+  end
 end
